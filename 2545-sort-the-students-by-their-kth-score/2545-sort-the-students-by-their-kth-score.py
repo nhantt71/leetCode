4113e19c318d0eb426bd5c505762b62e2dd7fb24
nhantt71 <125555189+nhantt71@users.noreply.github.com>
2023-07-14 18:24:28 +0700

    Time: 393 ms (90.34%), Space: 19 MB (19.89%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
@@ -1,13 +1,13 @@
-class Solution(object):
+class Solution:
     def sortTheStudents(self, score, k):
-        """
-        :type score: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-
-        def get_max_value(sublist):
-            return sublist[k]
-
-        score.sort(key=get_max_value, reverse=True)
-        return score
+        index_dict = {}
+        for index,item in enumerate(score):
+            index_dict[item[k]] = index                    
+        sort_key = sorted(index_dict.keys(),reverse=True)   
+        output = []
+        for key in sort_key:
+            output.append(score[index_dict[key]])          
+        return output
+    
+    
+#using enumerate example
